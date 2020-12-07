54d1e6e9e2ef8146e9e124ac026f32fd41d8577f
Oliver Pinter <oliver.pntr@gmail.com>
2020-12-07 16:29:29 +0100

    add bin scripts
    Signed-off-by: Oliver Pinter <oliver.pntr@gmail.com>

---
+++ home/bin/realpath.c
@@ -0,0 +1,26 @@
+#include <sys/param.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char** argv)
+{
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: realpath <path>\n");
+		return (1);
+	}
+
+	char* path = argv[1];
+	char result[PATH_MAX];
+
+	if (realpath(path, result)) {
+		printf("%s\n", result);
+		return (0);
+	} else {
+		fprintf(stderr, "Bad path: %s\n", result);
+		return (1);
+	}
+
+	return (0);
+}
